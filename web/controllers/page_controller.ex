b11eddd1adc3a2cbf674f45cef52897b2cd54928
bryan <bryan.hunt@erlang-solutions.com>
2019-01-31 16:18:01 +0000

    foo

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,16 +2,30 @@ defmodule FileUpload.PageController do
   use FileUpload.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html", token: get_csrf_token()
+    render(conn, "index.html", token: get_csrf_token())
   end
 
+  def upload(
+        conn,
+        %{
+          "fileToUpload" => %Plug.Upload{
+            path: path
+          }
+        } = params
+      ) do
+    IO.puts("upload:  #{inspect(path)}")
 
-def foretell(conn, %{"name" => name}) do
+    File.copy!(path, "/tmp/output")
+
+    render(conn, "index.html", token: get_csrf_token())
+  end
+
+  def foretell(conn, %{"name" => name}) do
     prophecies = [
       "Nothing will be good, #{name}",
       "You don't need to think about the future, #{name}. You don't have any"
     ]
-    
-    text conn, Enum.random(prophecies)
-end
+
+    text(conn, Enum.random(prophecies))
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -2,22 +2,23 @@ defmodule FileUpload.Router do
   use FileUpload.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", FileUpload do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    post "/", PageController, :index
+    get("/", PageController, :index)
+    post("/", PageController, :upload)
   end
 
   # Other scopes may use custom stacks.
